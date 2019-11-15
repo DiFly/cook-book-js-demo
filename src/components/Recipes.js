d641ad9ef3f69ad9f3f2b7d478d15e5af9f0acd4
difly <kryshen.yaroslav@gmail.com>
2019-11-16 01:27:49 +0200

    Create new simple component to view recipe

---
+++ src/components/Recipes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Recipes = ({recipes}) => {
+    let  formatter = new Intl.DateTimeFormat("en", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    return (
+        <div className="row">
+        {recipes.map(recipe => (
+                <div
+                    className="card col-sm-4 m-0"
+                    key={recipe.id}
+                >
+                    <div className="card-body">
+                        <h5 className="card-title">{recipe.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Created {formatter.format(recipe.create)}</h6>
+                        <p className="card-text">{recipe.description}</p>
+                        <p className="card-text"><small className="text-muted">Last updated {formatter.format(recipe.change)}</small></p>
+                        <a href="#" className="card-link">See all recipe changes</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
--- src/pages/RecipeList.js
+++ src/pages/RecipeList.js
@@ -1,9 +1,23 @@
 import React, {Fragment} from 'react';
+import {Form} from "../components/Form";
+import {Recipes} from "../components/Recipes";
 
 export const RecipeList = () => {
+    const recipes = new Array(3)
+        .fill('')
+        .map((_, i) => ({
+            id: i,
+            title: `R Title ${i}`,
+            create: Date.now(),
+            description: 'Some text blablabla',
+            change: Date.now()
+        }))
+
     return (
         <Fragment>
-            <h1>Recipe List</h1>
+            <Form/>
+            <hr/>
+            <Recipes recipes={recipes}/>
         </Fragment>
     )
 }
