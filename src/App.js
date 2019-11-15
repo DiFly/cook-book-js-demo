c22997638149d7bcb6890d47dfb1fee4d14fb301
difly <kryshen.yaroslav@gmail.com>
2019-11-15 23:43:29 +0200

    Create base page (blank of web page: home, recipe list, about)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Navbar} from "./components/Navbar";
+import {RecipeList} from "./pages/RecipeList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <div className="container pt-4">
+                <Switch>
+                    <Route path={'/'} exact component={Home} />
+                    <Route path={'/recipes'} exact component={RecipeList} />
+                    <Route path={'/about'} exact component={About} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/RecipeList.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react';
+
+export const RecipeList = () => {
+    return (
+        <Fragment>
+            <h1>Recipe List</h1>
+        </Fragment>
+    )
+}
